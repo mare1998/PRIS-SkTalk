8e236caa7cadd857733ba8dccefa6e98c8dd84f7
Stefan <zivkov38@gmail.com>
2022-04-17 00:52:57 +0200

    @T4711:R dodavanje dodajKategoriju(String naziv)

--- SpringProject/TalkWEB/src/main/java/com/example/demo/controller/AdministratorController.java
+++ SpringProject/TalkWEB/src/main/java/com/example/demo/controller/AdministratorController.java
@@ -3,10 +3,14 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.repository.KategorijaRepository;
 import com.example.demo.repository.KorisnikRepository;
 import com.example.demo.repository.PredavacRepository;
+
+import model.Kategorija;
 
 @Controller
 @RequestMapping(value="/administrator")
@@ -21,7 +25,19 @@ public class AdministratorController {
 	@Autowired
 	KorisnikRepository korisnikRepo;
 	
-
+	@RequestMapping(value = "/dodajKategoriju", method = RequestMethod.POST)
+	public boolean dodajKategoriju(@RequestParam String naziv) {
+		Kategorija kategorija = kategorijaRepo.findKategorija(naziv);
+		if(kategorija == null) {
+			Kategorija k = new Kategorija();
+			k.setNaziv(naziv);
+			k = kategorijaRepo.save(k);
+			if(k != null) {
+				return true;
+			}
+		}
+		return false;
+	}
 	
 
 }
