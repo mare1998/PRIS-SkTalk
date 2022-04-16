cfb642996c717f20adad79fca4d52f00ca9d9a92
Stefan <zivkov38@gmail.com>
2022-04-17 00:57:40 +0200

    @T4712:R dodavanje sviKursevi()

--- SpringProject/TalkWEB/src/main/java/com/example/demo/controller/PolaznikController.java
+++ SpringProject/TalkWEB/src/main/java/com/example/demo/controller/PolaznikController.java
@@ -1,10 +1,15 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.repository.KursRepository;
+
+import model.Kur;
 
 @Controller
 @RequestMapping(value="/polaznik")
@@ -12,5 +17,10 @@ public class PolaznikController {
 	
 	@Autowired
 	KursRepository kursRepo;
+	
+	@RequestMapping(value="/sviKursevi", method=RequestMethod.GET)
+	public List<Kur> sviKursevi(){
+		return kursRepo.findAll();
+	}
 
 }
