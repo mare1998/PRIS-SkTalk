26dd7c4eb12eae6be60be7604e783cf14d10397b
Stefan <zivkov38@gmail.com>
2022-04-17 01:07:45 +0200

    @T4713:R dodavanje prikazKursa(int id)

--- SpringProject/TalkWEB/src/main/java/com/example/demo/controller/PolaznikController.java
+++ SpringProject/TalkWEB/src/main/java/com/example/demo/controller/PolaznikController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.repository.KursRepository;
 
@@ -21,6 +22,16 @@ public class PolaznikController {
 	@RequestMapping(value="/sviKursevi", method=RequestMethod.GET)
 	public List<Kur> sviKursevi(){
 		return kursRepo.findAll();
+	}
+	
+	@RequestMapping(value="/prikazKursa", method=RequestMethod.GET)
+	public Kur prikazKursa(@RequestParam int id) {
+		Kur k = kursRepo.getById(id);
+		if(k != null) {
+			return k;
+		}else {
+			return null;
+		}
 	}
 
 }
