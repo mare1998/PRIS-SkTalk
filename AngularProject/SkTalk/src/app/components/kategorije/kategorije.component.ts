13816df2bb12e4c60b32d445fc71b62be299ade8
Ivana <imilutinovic119@gmail.com>
2022-04-17 14:35:34 +0200

    sunday commit

--- AngularProject/SkTalk/src/app/components/kategorije/kategorije.component.ts
+++ AngularProject/SkTalk/src/app/components/kategorije/kategorije.component.ts
@@ -3,6 +3,8 @@ import { Kategorija } from 'src/app/model/kategorija';
 import { Kurs } from 'src/app/model/kurs';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
+import { KategorijaService } from 'src/app/services/kategorija.service';
+import { KursService } from 'src/app/services/kurs.service';
 
 @Component({
   selector: 'app-kategorije',
@@ -19,9 +21,22 @@ export class KategorijeComponent implements OnInit {
   public selectedKat: Kategorija = new Kategorija()
   public trazeniKurs: Kurs | undefined
   private reloadKategorijasList: Subject<any> = new Subject();
-  constructor() { }
+
+  constructor(/*private kategorijaService: KategorijaService, private knjigaService: KursService, private router:Router*/) { }
 
   ngOnInit(): void {
+  }
+
+  onCreate(){
+    this.reloadKategorijasList.next();
+  }
+
+  kurseviZaKategoriju(idKategorija:number){
+
+  }
+
+  nadjiKurs(searchForm:any){
+
   }
 
 }
