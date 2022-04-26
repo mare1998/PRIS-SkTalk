b2abdb631ad244ab9dbdd5d80ef2bd874a0bf8a6
Ivana <imilutinovic119@gmail.com>
2022-04-26 12:57:33 +0200

    commit 12:57

--- AngularProject/SkTalk/src/app/app.component.ts
+++ AngularProject/SkTalk/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Kategorija } from './model/kategorija';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SkTalk';
+
+  public kategorije: Kategorija[] | undefined;
+  
 }
