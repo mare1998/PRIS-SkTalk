51eff2b8ef730c7df46b8e7717c8eeca0a9d3ff3
Stefan <zivkov38@gmail.com>
2022-04-29 19:33:25 +0200

    @T4835:R @T4834:R

--- SpringProject/TalkWEB/src/main/java/com/example/demo/controller/PolaznikController.java
+++ SpringProject/TalkWEB/src/main/java/com/example/demo/controller/PolaznikController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.repository.KategorijaRepository;
 import com.example.demo.repository.KursRepository;
 import com.example.demo.repository.PredavacRepository;
 
@@ -29,6 +30,9 @@ public class PolaznikController {
 	
 	@Autowired
 	PredavacRepository predavacRepo;
+	
+	@Autowired
+	KategorijaRepository kategorijaRepo;
 	
 	@RequestMapping(value="/sviKursevi", method=RequestMethod.GET)
 	public ResponseEntity<List<Kur>> sviKursevi(){
@@ -56,5 +60,12 @@ public class PolaznikController {
 	public ResponseEntity<Kur> pronadjiKursPoNazivu(@PathVariable String nazivKursa) {
 		Kur k = kursRepo.findByNaziv(nazivKursa);
 		return new ResponseEntity<Kur>(k, HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/kurseviZaKategoriju/{idKursa}")
+	public ResponseEntity<List<Kur>> kurseviZaKategoriju(@PathVariable Integer id){
+		Kategorija k = kategorijaRepo.getById(id);
+		List<Kur> kursevi = kursRepo.findByKategorija(k);
+		return new ResponseEntity<List<Kur>>(kursevi, HttpStatus.OK);
 	}
 }
